348dfaf74103cd8251a74743034d956e63dcd754
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-02-23 12:52:41 +0600

    Text style and padding

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
-      title:'Test App',
+    return MaterialApp(
+      title: 'Test App',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Demo  Page'),
           backgroundColor: Colors.amberAccent,
         ),
-        body: Text('Hello Flutter'),
+        body: Padding(
+          
+          padding: EdgeInsets.all(20),
+          child: Text(
+            'The qucik brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog',
+            textAlign: TextAlign.justify,
+            textDirection: TextDirection.rtl,
+            style: TextStyle(fontSize: 20, shadows: <Shadow>[
+              Shadow(
+                offset: Offset(2, 2),
+                blurRadius: 5.0,
+                color: Colors.grey,
+              )
+            ]),
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){},
+          onPressed: () {},
           child: Icon(Icons.add),
           foregroundColor: Colors.blueGrey,
           hoverColor: Colors.lightBlue,
-          focusElevation:5.0,
+          focusElevation: 5.0,
         ),
       ),
     );
